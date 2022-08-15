7adf0d06ebdc377dc70c92e9149d35a608b0d293
NikolaVetnic <nikola.pacek.vetnic@gmail.com>
2022-08-15 18:39:27 +0200

    Interpolation.

--- 02-13_interpolation/main.cpp
+++ 02-13_interpolation/main.cpp
@@ -35,11 +35,14 @@ static const char* vShader = "                                  \n\
                                                                 \n\
     layout (location = 0) in vec3 pos;                          \n\
                                                                 \n\
+    out vec4 vCol;                                              \n\
+                                                                \n\
     uniform mat4 model;                                         \n\
                                                                 \n\
     void main()                                                 \n\
     {                                                           \n\
         gl_Position = model * vec4(pos, 1.0f);                  \n\
+        vCol = vec4(clamp(pos, 0.0f, 1.0f), 1.0f);              \n\
     }                                                           \n\
 ";
 
@@ -47,11 +50,13 @@ static const char* vShader = "                                  \n\
 static const char* fShader = "                                  \n\
     #version 330                                                \n\
                                                                 \n\
+    in vec4 vCol;                                               \n\
+                                                                \n\
     out vec4 color;                                             \n\
                                                                 \n\
     void main()                                                 \n\
     {                                                           \n\
-        color = vec4(1.0f, 0.0f, 0.0f, 1.0f);                   \n\
+        color = vCol;                                           \n\
     }                                                           \n\
 ";
 
@@ -244,9 +249,9 @@ int main()
         glm::mat4 model = glm::mat4(1.0f); // initialised to identity matrix
         
         // order of operations is important
-        model = glm::rotate(model, currAngle * toRadians, glm::vec3(0.0f, 0.0f, 1.0f));
-        model = glm::translate(model, glm::vec3(triOffset, triOffset, 0.0f));
-        model = glm::scale(model, glm::vec3(currSize, currSize, 0.0f));
+        // model = glm::rotate(model, currAngle * toRadians, glm::vec3(0.0f, 0.0f, 1.0f));
+        // model = glm::translate(model, glm::vec3(triOffset, triOffset, 0.0f));
+        model = glm::scale(model, glm::vec3(0.4f, 0.4f, 0.0f));
 
         glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
 
