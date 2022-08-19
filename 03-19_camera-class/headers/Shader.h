91de6657b4a447308feb155b70a4b1a6611cc7ac
NikolaVetnic <62119280+NikolaVetnic@users.noreply.github.com>
2022-08-19 15:29:08 +0300

    Camera class. (#13)
    * Branch start.
    
    * Working keyboard camera input.
    
    * Delta time implemented.
    
    * Mousse control implemented.

---
+++ 03-19_camera-class/headers/Shader.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <fstream>
+#include <iostream>
+#include <stdio.h>
+#include <string.h>
+
+#include <GL/glew.h>
+
+class Shader
+{
+    public:
+        Shader();
+
+        void CreateFromString(const char *vertexCode, const char *fragmentCode);
+        void CreateFromFiles(const char *vertexLocation, const char *fragmentLocation);
+
+        std::string ReadFile(const char *fileLocation);
+
+        GLuint GetProjectionLocation();
+        GLuint GetModelLocation();
+        GLuint GetViewLocation();
+
+        void UseShader();
+        void ClearShader();
+
+        ~Shader();
+
+    private:
+        GLuint shaderId, uniformProjection, uniformModel, uniformView;
+
+        void CompileShader(const char *vertexCode, const char *fragmentCode);
+        void AddShader(GLuint shaderProgram, const char* shaderCode, GLenum shaderType);
+};
